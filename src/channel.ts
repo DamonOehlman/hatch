4012ff9239cde2a0006282c29750bc6f9dc3db2b
Damon Oehlman <damon.oehlman@gmail.com>
2018-12-16 19:09:36 +1100

    .

--- src/channel.ts
+++ src/channel.ts
@@ -19,26 +19,4 @@ export class EventChannel {
   public close() {
     this.handlers.onClose();
   }
-
 }
-
-// function EventChannel(id) {
-//   if (! (this instanceof EventChannel)) {
-//     return new EventChannel(id);
-//   }
-
-//   this.id = id;
-//   this.baseArgs = ['hatch', id];
-// }
-
-// module.exports = EventChannel;
-
-// EventChannel.prototype.emit = function(name) {
-//   var extraArgs = [].slice.call(arguments, 1);
-
-//   // preped the base namespace to the event name
-//   name = this.baseArgs.concat(name.split('.')).join('.');
-
-//   // trigger the eve event
-//   eve.apply(eve, [name, this].concat(extraArgs));
-// };
